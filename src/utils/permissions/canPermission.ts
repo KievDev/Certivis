ac49e7a80bd63b87fbb33966456f3a57e393ea23
KievDev <kievdevops@gmail.com>
2025-05-28 12:41:55 -0300

    Plans limits
    Setting the limits of each plan

---
+++ src/utils/permissions/canPermission.ts
@@ -0,0 +1,56 @@
+"use server";
+
+import { auth } from "@/lib/auth";
+import { PlanDetailInfo } from "./get-plans";
+import prisma from "@/lib/prisma";
+
+export type PLAN_PROP = "BASIC" | "PROFESSIONAL" | "TRIAL" | "EXPIRED";
+
+interface ResultPermissionProp {
+  hasPermission: boolean;
+  PlanId: string;
+  expired: boolean;
+  plan: PlanDetailInfo | null;
+}
+
+interface CanPermissionProps {
+  type: string;
+}
+
+export async function canPermission({
+  type,
+}: CanPermissionProps): Promise<ResultPermissionProp> {
+  const session = await auth();
+
+  if (!session?.user?.id) {
+    return {
+      hasPermission: false,
+      PlanId: "EXPIRED",
+      expired: true,
+      plan: null,
+    };
+  }
+
+  const subscription = await prisma.subscription.findFirst({
+    where: {
+      userId: session?.user?.id,
+    },
+  });
+
+  switch (type) {
+    case "service":
+      return {
+        hasPermission: false,
+        PlanId: "EXPIRED",
+        expired: true,
+        plan: null,
+      };
+    default:
+      return {
+        hasPermission: false,
+        PlanId: "EXPIRED",
+        expired: true,
+        plan: null,
+      };
+  }
+}
