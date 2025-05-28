8bf5e015e9ebf42c1843c1749afd2c8ce34b6d29
KievDev <kievdevops@gmail.com>
2025-05-28 12:20:14 -0300

    Server function
    Made a server function to get the signature status of the client

--- src/app/(panel)/dashboard/plans/_components/subscription-detail.tsx
+++ src/app/(panel)/dashboard/plans/_components/subscription-detail.tsx
@@ -11,6 +11,8 @@ import {
 } from "@/components/ui/card";
 import { Subscription } from "@/generated/prisma";
 import { subscriptionPlans } from "@/utils/plans";
+import { createPortalCustomer } from "../_actions/create-portal-customer";
+import { toast } from "sonner";
 
 interface SubscriptionDetailProps {
   subscription: Subscription;
@@ -21,7 +23,16 @@ export function SubscriptionDetail({ subscription }: SubscriptionDetailProps) {
     (plan) => plan.id === subscription.plan
   );
 
-  async function handleManageSubscription() {}
+  async function handleManageSubscription() {
+    const portal = await createPortalCustomer();
+
+    if (portal.error) {
+      toast.error("Ocorreu um erro ao criar o portal de assinatura");
+      return;
+    }
+
+    window.location.href = portal.sessionId;
+  }
 
   return (
     <Card className="w-full mx-auto">
