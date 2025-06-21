4d955a1787d1988227d6d8082ed071390c9f112e
KievDev <kievdevops@gmail.com>
2025-06-21 13:04:08 -0300

    Updated home page
    Show star on premium clinics

--- src/app/(public)/_components/professionals.tsx
+++ src/app/(public)/_components/professionals.tsx
@@ -10,10 +10,15 @@ import Image from "next/image";
 import photoImg from "../../../../public/foto1.png";
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
-import { User } from "@/generated/prisma";
+import { Prisma, User } from "@/generated/prisma";
+import { PremiumCardBadge } from "./premium-badge";
+
+type UserWithSubscription = Prisma.UserGetPayload<{
+  include: { subscription: true };
+}>;
 
 interface ProfessionalsProps {
-  professionals: User[];
+  professionals: UserWithSubscription[];
 }
 
 export function Professionals({ professionals }: ProfessionalsProps) {
@@ -41,19 +46,22 @@ export function Professionals({ professionals }: ProfessionalsProps) {
                       alt="Foto da clínica"
                       fill
                     />
+                    {clinic?.subscription?.status === "active" &&
+                      clinic?.subscription?.plan === "PROFESSIONAL" && (
+                        <PremiumCardBadge />
+                      )}
                   </div>
                 </div>
                 <div
-                  className="p-4 space-y-4" // space-y-4 = space between elements vertically 4
+                  className="p-4 space-y-4 min-h-[160px] flex flex-col justify-between" // space-y-4 = space between elements vertically 4
                 >
                   <div className="flex items-center justify-between">
                     <div>
                       <h3 className="font-semibold">{clinic.name}</h3>
-                      <p className="text-sm text-gray-500">
+                      <p className="text-sm text-gray-500 line-clamp-2">
                         {clinic.address ?? "Endereço não informado"}
                       </p>
                     </div>
-                    <div className="w-2.5 h-2.5 rounded-full bg-emerald-500"></div>
                   </div>
                   <Link
                     href={`/clinica/${clinic.id}`}
